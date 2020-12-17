637309416d0ca34ecbd097830d1fe60ec5f84665
nahira <Mitsuhiro_Nahira@n.t.rd.honda.co.jp>
2020-12-18 08:47:54 +0900

    sslの無効化処理の位置を変更

--- bot.py
+++ bot.py
@@ -1,12 +1,10 @@
 # ライブラリの読み込み
-
 import ssl
-ssl._create_default_https_context = ssl._create_unverified_context
-# https://qiita.com/shutokawabata0723/items/9733a7e640a175c23f39
-
 from slackbot.bot import Bot
 
 def main():
+    ssl._create_default_https_context = ssl._create_unverified_context
+    # https://qiita.com/shutokawabata0723/items/9733a7e640a175c23f39
 
     # ボットの起動
     bot = Bot()
